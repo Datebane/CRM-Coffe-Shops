53bc3c42e43537557f94bd8d7bdb498017d8b86f
Dominik Korniichuk <kordominik1@gmail.com>
2024-02-27 23:16:30 +0200

    request to the api output of the first photo

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,43 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [photos, setPhotos] = useState([]);
+
+  useEffect(() => {
+    const fetchPhotos = async () => {
+      try {
+        const response = await axios.get("https://api.unsplash.com/photos", {
+          params: { client_id: "yx942CVK5iBIR-aYFffB0ks9DNCZ7e5LOV4_qDTJi6A" },
+        });
+        setPhotos(response.data);
+      } catch (error) {
+        console.error("Error fetching photos:", error);
+      }
+    };
+
+    fetchPhotos();
+  }, []);
 
   return (
-    <>
-      
-    </>
-  )
-}
+    <div>
+      <h2>Unsplash Photos</h2>
+      <ol>
+        {photos.map(
+          (photo: {
+            id: string;
+            urls: { small: string };
+            alt_description: string;
+          }) => (
+            <li key={photo.id}>
+              <img src={photo.urls.small} alt={photo.alt_description} />
+            </li>
+          )
+        )}
+      </ol>
+    </div>
+  );
+};
 
-export default App
+export default App;
